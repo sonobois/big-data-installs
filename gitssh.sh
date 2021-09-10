32f28f797ed5f4a3af01b2809a30b9a03104bf5d
Aditeya <aditeya.baral@gmail.com>
2021-09-10 20:16:18 +0530

    Setup git SSH

--- gitssh.sh
+++ gitssh.sh
@@ -3,3 +3,5 @@ mkdir $HOME/.ssh
 ssh-keygen -t rsa -b 4096 $EMAIL
 ssh-add ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub
+echo "Visit https://github.com/settings/keys. Create a new SSH key and paste the above key there"
+echo "Then run this on the terminal: ssh -T git@github.com"
